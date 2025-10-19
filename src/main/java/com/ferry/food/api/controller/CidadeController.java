43a1d07d391c29f4751a979267afaee1fe265bee
Ricardo <rick_ferry1@hotmail.com>
2025-10-19 01:38:25 -0300

    CRUD Cidade

---
+++ src/main/java/com/ferry/food/api/controller/CidadeController.java
@@ -0,0 +1,68 @@
+package com.ferry.food.api.controller;
+
+import com.ferry.food.api.service.CidadeService;
+import com.ferry.food.domain.exception.EntityInUseException;
+import com.ferry.food.domain.exception.MyEntityNotFoundException;
+import com.ferry.food.domain.model.Cidade;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/cidades")
+public class CidadeController {
+    private final CidadeService cidadeService;
+
+    @GetMapping
+    public ResponseEntity<List<Cidade>> listar() {
+        return ResponseEntity.ok(cidadeService.listar());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<?> buscarPorId(@PathVariable Long id) {
+        try {
+            Cidade cidade = cidadeService.buscarPorId(id);
+            return ResponseEntity.ok(cidade);
+        } catch (MyEntityNotFoundException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+        }
+    }
+
+    @PostMapping
+    public ResponseEntity<?> cadastrar(@RequestBody Cidade cidade, UriComponentsBuilder builder) {
+        try {
+            Cidade cidadeSalva = cidadeService.salvar(cidade);
+            return ResponseEntity
+                    .created(builder.path("/{id}")
+                            .buildAndExpand(cidadeSalva.getId()).toUri())
+                    .body(cidadeSalva);
+        } catch (MyEntityNotFoundException e) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+        }
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<?> atualizar(@PathVariable Long id, @RequestBody Cidade cidade) {
+        try {
+            Cidade cidadeAtualizada = cidadeService.atualizar(id, cidade);
+            return ResponseEntity.ok(cidadeAtualizada);
+        } catch (MyEntityNotFoundException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> deletar(@PathVariable Long id) {
+        try {
+            cidadeService.deletar(id);
+            return ResponseEntity.noContent().build();
+        } catch (MyEntityNotFoundException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+        }
+    }
+}
---
+++ src/main/java/com/ferry/food/api/service/CidadeService.java
@@ -0,0 +1,79 @@
+package com.ferry.food.api.service;
+
+import com.ferry.food.domain.exception.MyEntityNotFoundException;
+import com.ferry.food.domain.model.Cidade;
+import com.ferry.food.domain.model.Estado;
+import com.ferry.food.domain.repository.CidadeRepository;
+import com.ferry.food.domain.repository.EstadoRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.BeanUtils;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+import static java.lang.String.format;
+
+@Service
+@RequiredArgsConstructor
+public class CidadeService {
+    public static final String CIDADE_DE_CODIGO_D_NAO_ENCONTRADO = "Cidade de código %d não encontrado";
+
+    private final CidadeRepository cidadeRepository;
+    private final EstadoRepository estadoRepository;
+
+    @Transactional(readOnly = true)
+    public List<Cidade> listar() {
+        return cidadeRepository.listarCidades();
+    }
+
+    @Transactional(readOnly = true)
+    public Cidade buscarPorId(Long id) {
+        Cidade cidade = cidadeRepository.buscarCidade(id);
+        if (cidade != null) {
+            return cidade;
+        }
+        throw new MyEntityNotFoundException(
+                format(CIDADE_DE_CODIGO_D_NAO_ENCONTRADO, id));
+    }
+
+    @Transactional
+    public Cidade salvar(Cidade cidade) {
+        Long estadoId = cidade.getEstado().getId();
+        Estado estado = estadoRepository.buscarEstado(estadoId);
+        if (estado == null) {
+            throw new MyEntityNotFoundException(
+                    format("Estado de código %d não encontrado", estadoId));
+        }
+        cidade.setEstado(estado);
+        return cidadeRepository.adicionarCidade(cidade);
+    }
+
+    @Transactional
+    public Cidade atualizar(Long id, Cidade cidade) {
+        Cidade cidadeAtual = cidadeRepository.buscarCidade(id);
+        if (cidadeAtual != null) {
+            Long estadoId = cidade.getEstado().getId();
+            Estado estado = estadoRepository.buscarEstado(estadoId);
+            if (estado == null) {
+                throw new MyEntityNotFoundException(
+                        format("Estado de código %d não encontrado", estadoId));
+            }
+            BeanUtils.copyProperties(cidade, cidadeAtual, "id", "estado");
+            cidadeAtual.setEstado(estado);
+            return cidadeRepository.adicionarCidade(cidadeAtual);
+        }
+        throw new MyEntityNotFoundException(
+                format(CIDADE_DE_CODIGO_D_NAO_ENCONTRADO, id));
+    }
+
+    @Transactional
+    public void deletar(Long id) {
+        Cidade cidade = cidadeRepository.buscarCidade(id);
+        if (cidade != null) {
+            cidadeRepository.removerCidade(cidade.getId());
+        }
+        throw new MyEntityNotFoundException(
+                format(CIDADE_DE_CODIGO_D_NAO_ENCONTRADO, id));
+    }
+}
