fff237100dfa420f27cb3365201e90c6b0c763fd
Ricardo <rick_ferry1@hotmail.com>
2025-10-26 00:19:31 -0300

    Primeira versao criteria

--- src/main/java/com/ferry/food/domain/repository/impl/RestauranteRepositoryImpl.java
+++ src/main/java/com/ferry/food/domain/repository/impl/RestauranteRepositoryImpl.java
@@ -1,13 +1,12 @@
 package com.ferry.food.domain.repository.impl;
 
 import com.ferry.food.domain.model.Restaurante;
+import lombok.var;
 import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
-import javax.persistence.TypedQuery;
 import java.math.BigDecimal;
-import java.util.HashMap;
 import java.util.List;
 
 @Repository
@@ -18,30 +17,9 @@ public class RestauranteRepositoryImpl implements RestauranteRepositoryQueries {
 
     @Override
     public List<Restaurante> find(String nome, BigDecimal taxaFreteInicial, BigDecimal taxaFreteFinal) {
-        StringBuilder jpql = new StringBuilder();
-        jpql.append("from Restaurante where 0 = 0 ");
-
-        HashMap<String, Object> parametros = new HashMap<>();
-
-        if (nome != null && !nome.isEmpty()) {
-            jpql.append("and nome like :nome ");
-            parametros.put("nome", "%" + nome + "%");
-        }
-
-        if (taxaFreteInicial != null) {
-            jpql.append("and taxaFrete >= :taxaFreteInicial ");
-            parametros.put("taxaFreteInicial", taxaFreteInicial);
-        }
-
-        if (taxaFreteFinal != null) {
-            jpql.append("and taxaFrete <= :taxaFreteFinal ");
-            parametros.put("taxaFreteFinal", taxaFreteFinal);
-        }
-
-        TypedQuery<Restaurante> query = em.createQuery(jpql.toString(), Restaurante.class);
-
-        parametros.forEach(query::setParameter);
-
-        return query.getResultList();
+        var criteriaBuilder = em.getCriteriaBuilder();
+        var criteriaQuery = criteriaBuilder.createQuery(Restaurante.class);
+        criteriaQuery.from(Restaurante.class);
+        return em.createQuery(criteriaQuery).getResultList();
     }
 }
