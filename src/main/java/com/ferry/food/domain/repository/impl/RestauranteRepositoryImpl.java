73441f97421c76dfdf08717740f8d1643cb22508
Ricardo <rick_ferry1@hotmail.com>
2025-10-26 00:26:33 -0300

    Segunda versao criteria

--- src/main/java/com/ferry/food/domain/repository/impl/RestauranteRepositoryImpl.java
+++ src/main/java/com/ferry/food/domain/repository/impl/RestauranteRepositoryImpl.java
@@ -6,6 +6,8 @@ import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
 import java.math.BigDecimal;
 import java.util.List;
 
@@ -19,7 +21,14 @@ public class RestauranteRepositoryImpl implements RestauranteRepositoryQueries {
     public List<Restaurante> find(String nome, BigDecimal taxaFreteInicial, BigDecimal taxaFreteFinal) {
         var criteriaBuilder = em.getCriteriaBuilder();
         var criteriaQuery = criteriaBuilder.createQuery(Restaurante.class);
-        criteriaQuery.from(Restaurante.class);
+        Root<Restaurante> root = criteriaQuery.from(Restaurante.class);
+
+        Predicate nomePredicate = criteriaBuilder.like(root.get("nome"), "%" + nome + "%");
+        Predicate taxaFreteInicialPredicate = criteriaBuilder.greaterThanOrEqualTo(root.get("taxaFrete"), taxaFreteInicial);
+        Predicate taxaFreteFinalPredicate = criteriaBuilder.lessThanOrEqualTo(root.get("taxaFrete"), taxaFreteFinal);
+
+        criteriaQuery.where(nomePredicate, taxaFreteInicialPredicate, taxaFreteFinalPredicate);
+
         return em.createQuery(criteriaQuery).getResultList();
     }
 }
